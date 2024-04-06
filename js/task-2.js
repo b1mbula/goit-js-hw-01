cec31d8cb59d4999c554d741e4b2bfee345c8f99
b1mbula <danja.rubeghanskij@gmail.com>
2024-04-06 14:40:18 +0200

    hw

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+function getShippingMessage(country, price, deliveryFee) {
+  const totalPrice = `Shipping to ${country} will cost ${
+    price + deliveryFee
+  } credits`;
+  return totalPrice;
+}
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Germany", 80, 20));
+console.log(getShippingMessage("Sweden", 100, 20));
